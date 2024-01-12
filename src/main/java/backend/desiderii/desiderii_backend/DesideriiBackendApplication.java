d4a552a470c6657ed2a82d1b4962bee3f646c79c
Misaka19986 <1657512446@qq.com>
2024-01-12 22:06:06 +0800

    update: env init

---
+++ src/main/java/backend/desiderii/desiderii_backend/DesideriiBackendApplication.java
@@ -0,0 +1,13 @@
+package backend.desiderii.desiderii_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesideriiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesideriiBackendApplication.class, args);
+    }
+
+}
