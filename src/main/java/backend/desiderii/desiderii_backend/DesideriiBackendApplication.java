33ffa56946f5fa0142f101399b8bb12f28a21899
Misaka19986 <1657512446@qq.com>
2024-01-12 22:53:25 +0800

    update: hello world

--- src/main/java/backend/desiderii/desiderii_backend/DesideriiBackendApplication.java
+++ src/main/java/backend/desiderii/desiderii_backend/DesideriiBackendApplication.java
@@ -2,12 +2,11 @@ package backend.desiderii.desiderii_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class })
 public class DesideriiBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DesideriiBackendApplication.class, args);
     }
-
 }
---
+++ src/main/java/backend/desiderii/desiderii_backend/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package backend.desiderii.desiderii_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("Success!\n");
+        return "hello world";
+    }
+}
