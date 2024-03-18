54b25e2361d2e99e202ad9a72d609051854c1e42
Roxy <1657512446@qq.com>
2024-03-18 22:50:25 +0800

    Update settings.py

--- Desiderii_backend_p/settings.py
+++ Desiderii_backend_p/settings.py
@@ -115,14 +115,21 @@ WSGI_APPLICATION = 'Desiderii_backend_p.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'desiderii_backend_p',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'desiderii_backend_p',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
